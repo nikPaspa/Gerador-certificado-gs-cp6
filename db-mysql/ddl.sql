99dc4177741cb0ea20ff76f18e1a5c6f3c249ef6
Nikolas Paspaltzis <62848473+nikPaspa@users.noreply.github.com>
2024-10-30 22:39:20 -0300

    first commit

---
+++ db-mysql/ddl.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS Gerador-certificado-gs-cp6;
+USE Gerador-certificado-gs-cp6;
+
+CREATE TABLE IF NOT EXISTS diplomas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome_aluno VARCHAR(255),
+    data_conclusao DATE,
+    nome_curso VARCHAR(255),
+    nacionalidade VARCHAR(255),
+    naturalidade VARCHAR(255),
+    data_nascimento DATE,
+    numero_rg VARCHAR(20),
+    data_emissao DATE,
+    template_diploma TEXT
+);
+
+CREATE TABLE IF NOT EXISTS assinaturas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    diploma_id INT,
+    cargo VARCHAR(255),
+    nome VARCHAR(255),
+    FOREIGN KEY (diploma_id) REFERENCES diplomas(id)
+);
+
